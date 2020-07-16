207fa8e4b70ce31b48cc5b4591c251d16caa672f
xbkaishui <xbkaishui@126.com>
2020-07-16 10:54:30 +0800

    fix  sql parser  issue to support  emoji

---
+++ shardingsphere-sql-parser/shardingsphere-sql-parser-test/src/test/java/org/apache/shardingsphere/sql/parser/integrate/engine/SqlParserTest.java
@@ -0,0 +1,26 @@
+/*
+ * credigo.com Inc.
+ * Copyright (c) 2018-2020 All Rights Reserved.
+ */
+package org.apache.shardingsphere.sql.parser.integrate.engine;
+
+import org.apache.shardingsphere.sql.parser.SQLParserEngineFactory;
+import org.apache.shardingsphere.sql.parser.sql.statement.SQLStatement;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author xbkaishui
+ * @version $Id: SqlParserTest.java,  2020-07-15 5:17 PM xbkaishui Exp $$
+ */
+public class SqlParserTest {
+    
+    @Test
+    public void testEmoji() throws Exception {
+        System.out.println("璐小果\uD83C\uDF4E我\uD83D\uDE00\uD869\uDEA5徐学习學習~\uD83D\uDE4B\u200D♀️\uD835\uDCD2\uD835\uDCF8\uD835\uDCEC\uD835\uDCEA\uD835\uDCD2\uD835\uDCF8\uD835\uDCF5\uD835\uDCEAasdfas!@#$%^123123d,.".length());
+        String databaseType = "MySQL";
+        String sql = "INSERT INTO t_user_third_account ( third_account_id,nickname)values(10,'璐小果\uD83C\uDF4E我\uD83D\uDE00\uD869\uDEA5徐学习學習~\uD83D\uDE4B\u200D♀️\uD835\uDCD2\uD835\uDCF8\uD835\uDCEC\uD835\uDCEA\uD835\uDCD2\uD835\uDCF8\uD835\uDCF5\uD835\uDCEAasdfas!@#$%^123123d,.')";
+        SQLStatement actual = SQLParserEngineFactory.getSQLParserEngine(databaseType).parse(sql, false);
+        Assert.assertNotNull(actual);
+    }
+}
