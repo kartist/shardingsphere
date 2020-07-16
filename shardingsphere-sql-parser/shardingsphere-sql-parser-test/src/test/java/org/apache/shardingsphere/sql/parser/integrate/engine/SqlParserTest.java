9ca01dacab60a1e6ac9c7ab1e27b21589119bf8e
xbkaishui <xbkaishui@126.com>
2020-07-16 15:38:01 +0800

    change emoji impl logic

--- shardingsphere-sql-parser/shardingsphere-sql-parser-test/src/test/java/org/apache/shardingsphere/sql/parser/integrate/engine/SqlParserTest.java
+++ shardingsphere-sql-parser/shardingsphere-sql-parser-test/src/test/java/org/apache/shardingsphere/sql/parser/integrate/engine/SqlParserTest.java
@@ -17,7 +17,14 @@ public class SqlParserTest {
     
     @Test
     public void testEmoji() throws Exception {
-        System.out.println("璐小果\uD83C\uDF4E我\uD83D\uDE00\uD869\uDEA5徐学习學習~\uD83D\uDE4B\u200D♀️\uD835\uDCD2\uD835\uDCF8\uD835\uDCEC\uD835\uDCEA\uD835\uDCD2\uD835\uDCF8\uD835\uDCF5\uD835\uDCEAasdfas!@#$%^123123d,.".length());
+        System.out.println("汉".length());
+        String str = "璐小果\uD83C\uDF4E我\uD83D\uDE00\uD869\uDEA5徐学习學習~\uD83D\uDE4B\u200D♀️\uD835\uDCD2\uD835\uDCF8\uD835\uDCEC\uD835\uDCEA\uD835\uDCD2\uD835\uDCF8\uD835\uDCF5\uD835\uDCEAasdfas!@#$%^123123d,.";
+        System.out.println("璐小果\uD83C\uDF4E".length());
+        int codePoint = (Character.codePointAt(str, 3));
+        int count = Character.charCount(codePoint);
+        System.out.println(str.charAt(3));
+        System.out.println(count);
+        System.out.println(str.length());
         String databaseType = "MySQL";
         String sql = "INSERT INTO t_user_third_account ( third_account_id,nickname)values(10,'璐小果\uD83C\uDF4E我\uD83D\uDE00\uD869\uDEA5徐学习學習~\uD83D\uDE4B\u200D♀️\uD835\uDCD2\uD835\uDCF8\uD835\uDCEC\uD835\uDCEA\uD835\uDCD2\uD835\uDCF8\uD835\uDCF5\uD835\uDCEAasdfas!@#$%^123123d,.')";
         SQLStatement actual = SQLParserEngineFactory.getSQLParserEngine(databaseType).parse(sql, false);
